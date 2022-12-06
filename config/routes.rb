71b9ff39b850abe61b89e8624770f0dacf8a94f4
XXXXXX <XXXXX@example.com>
2022-12-06 11:56:51 +0900

    end 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help',  as:'helf'
+  get '/about', to: 'static_pages#about'
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
 end
