ee8a2c48b803a8007c5c37053894bdd5ba04cc3d
XXXXXX <XXXXX@example.com>
2022-12-07 14:52:45 +0900

    end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'session/new'
   resources :users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help',  as:'helf'
   get '/about', to: 'static_pages#about'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   root 'static_pages#home'
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SessionControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get session_new_url
+    get login_path
     assert_response :success
   end
 end
