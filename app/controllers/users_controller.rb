83b99c4745faee464b744a7019da5d0e9c5b6e0f
XXXXXX <XXXXX@example.com>
2022-12-07 13:03:53 +0900

    start 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:succsess] = "succsess create user"
-      redirect_to user_url(@user)
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                :password_confirmation, :emp_no)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'session/new'
   resources :users
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help',  as:'helf'
   get '/about', to: 'static_pages#about'
